09f35beb317cab2c15c8fc45c41c126349931e8e
Footdablit2310 <kolayparamita@gmail.com>
2026-06-22 12:36:00 +0530

    Added static fields from logging module for ease of use.

--- color_logger.py
+++ color_logger.py
@@ -22,6 +22,15 @@ class ColorFormatter(logging.Formatter):
         msg = super().format(record)
         return f"{color}{msg}{Style.RESET_ALL}"
 
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+WARNING = logging.WARNING
+WARN = logging.WARN
+ERROR = logging.ERROR
+ERR = logging.ERROR
+CRITICAL = logging.CRITICAL
+FATAL = logging.FATAL
+
 # Create logger
 def create_logger(name: str, clear_handlers: bool = True, default_level: int = logging.DEBUG) -> logging.Logger:
     logger = logging.getLogger(name)
--- color_logger.pyi
+++ color_logger.pyi
@@ -1,4 +1,13 @@
 import logging
 
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+WARNING = logging.WARNING
+WARN = logging.WARN
+ERROR = logging.ERROR
+ERR = logging.ERROR
+CRITICAL = logging.CRITICAL
+FATAL = logging.FATAL
+
 def create_logger(name: str, clear_handlers: bool = True, default_level: int = logging.DEBUG) -> logging.Logger:
     """Creates a colored logger with the specified name and configuration."""
